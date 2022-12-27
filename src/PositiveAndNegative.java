b2f82dc830f331b4453d3b9b97e886393f05c19f
csshubhamcs <shubhamsinghhrx@gmail.com>
2022-12-27 17:45:02 +0530

    Positive Negative

---
+++ src/PositiveAndNegative.java
@@ -0,0 +1,10 @@
+public class PositiveAndNegative {
+    public static void main(String[] args) {
+        int n=-1;
+        if(n>0){
+            System.out.println(n+" Positive Number");
+        }else {
+            System.out.println(n+" Negative Number");
+        }
+    }
+}
