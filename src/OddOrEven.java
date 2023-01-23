58e21b0a7e7c127f4c17bcdc17e1e7b9d52d9665
csshubhamcs <shubhamsinghhrx@gmail.com>
2023-01-23 22:25:59 +0530

    Positive and Negative

---
+++ src/OddOrEven.java
@@ -0,0 +1,9 @@
+public class OddOrEven {
+    public static void main(String[] args) {
+        int n=10;
+        if(n%2==0)
+            System.out.println("Even Number");
+        else System.out.println("odd number");
+         }
+    }
+
