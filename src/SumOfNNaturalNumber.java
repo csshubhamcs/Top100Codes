78448108a54fabd92a64800ba8c3f5a688cafae9
csshubhamcs <shubhamsinghhrx@gmail.com>
2023-01-31 15:45:35 +0530

    sum of natural number

--- src/SumOfNNaturalNumber.java
+++ src/SumOfNNaturalNumber.java
@@ -1,2 +1,11 @@
 public class SumOfNNaturalNumber {
+    public static void main(String[] args) {
+        int n= 10;
+        int sum =0;
+
+        for (int i = 1; i<=n;i++){
+            sum = sum+i;
+        }
+        System.out.println("Addition = "+sum);
+    }
 }
